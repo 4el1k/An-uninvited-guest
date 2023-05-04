daade170e00b4158e2687d0bfeeaff9aec6c8a63
4e1ik <salavatalyukov@gmail.com>
2023-05-04 19:17:20 +0300

    added button click verification

--- main.py
+++ main.py
@@ -6,12 +6,8 @@ from telebot import types
 token = open("token.txt").readline()
 bot = telebot.TeleBot(token)
 
-user_ready_to_signin = False
-
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "HELLOOOO")
-
     bot.send_message(message.chat.id, 'Menu', reply_markup=make_button())
 
 
@@ -20,10 +16,11 @@ def send_menu(message):
 
 def send_description(message):
     markup = types.InlineKeyboardMarkup(row_width=1)
-    bot.send_message(message.chat.id, "Developer of this bot Salavat & Nikita")
+    bot.send_message(message.chat.id, "Developers of this bot Salavat & Nikita")
     buttonA = types.InlineKeyboardButton("back", callback_data="back")
     markup.add(buttonA)
     bot.edit_message_text(chat_id = message.chat.id,message_id=message.id, text='You choose description',reply_markup=markup)
+
 
 @bot.callback_query_handler(func =lambda call:True)
 def callback(call):
@@ -31,18 +28,23 @@ def callback(call):
         if call.data == "description":
             send_description(call.message)
         if call.data == "sign in":
-            pass;
+            global global_user_ready_to_signin
+            global_user_ready_to_signin=True
+            bot.send_message(call.message.chat.id, "Send your voice")
         if call.data == "back":
             send_menu(call.message)
 
 
 @bot.message_handler(content_types=['voice'])
 def download_voice_message(message):
-    file_info = bot.get_file(message.voice.file_id)
-    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(token, file_info.file_path))
-    with open(f'user_voice_from_chat_{message.chat.id}.ogg', 'wb') as f:
-        f.write(file.content)
-        f.close()
+    if (global_user_ready_to_signin):
+        file_info = bot.get_file(message.voice.file_id)
+        file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(token, file_info.file_path))
+        with open(f'user_voice_from_chat_{message.chat.id}.ogg', 'wb') as f:
+            f.write(file.content)
+            f.close()
+        bot.send_message(message.chat.id,"We got your voice")
+        start_message(message)
 
 def make_button():
     markup = types.InlineKeyboardMarkup(row_width=1)
